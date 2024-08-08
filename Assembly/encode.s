fa74ef0f6efc8c33df57452a371a4b31b10a0b49
JustinDrastil <105826553+JustinDrastil@users.noreply.github.com>
2024-08-08 15:47:27 -0700

    new assembly code from Encode.c that runs on the arm machine

--- Assembly/encode.s
+++ Assembly/encode.s
@@ -1,413 +1,423 @@
-        .arch armv5
-        .fpu softvfp
-        .eabi_attribute 20, 1
-        .eabi_attribute 21, 1
-        .eabi_attribute 23, 3
-        .eabi_attribute 24, 1
-        .eabi_attribute 25, 1
-        .eabi_attribute 26, 2
-        .eabi_attribute 30, 6
-        .eabi_attribute 18, 4
-        .file   "Encode.c"
-        .global lookup
-        .section        .rodata
-        .type   lookup, %object
-        .size   lookup, 4096
-lookup:
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "100\000"
-        .space  12
-        .ascii  "\000"
-        .space  15
-        .ascii  "00111111\000"
-        .space  7
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "11011001\000"
-        .space  7
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "001110\000"
-        .space  9
-        .ascii  "\000"
-        .space  15
-        .ascii  "101000\000"
-        .space  9
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "110110000\000"
-        .space  6
-        .ascii  "001100010\000"
-        .space  6
-        .ascii  "001100011\000"
-        .space  6
-        .ascii  "0011001001\000"
-        .space  5
-        .ascii  "1101100011\000"
-        .space  5
-        .ascii  "0011001010\000"
-        .space  5
-        .ascii  "0011001101\000"
-        .space  5
-        .ascii  "1101001100\000"
-        .space  5
-        .ascii  "110100100\000"
-        .space  6
-        .ascii  "0011000010\000"
-        .space  5
-        .ascii  "00110000000\000"
-        .space  4
-        .ascii  "0011000011\000"
-        .space  5
-        .ascii  "110100111\000"
-        .space  6
-        .ascii  "001100111\000"
-        .space  6
-        .ascii  "0011001000\000"
-        .space  5
-        .ascii  "1101001101\000"
-        .space  5
-        .ascii  "1101100010011\000"
-        .space  2
-        .ascii  "1101001010\000"
-        .space  5
-        .ascii  "110111100\000"
-        .space  6
-        .ascii  "110111101\000"
-        .space  6
-        .ascii  "00110010110\000"
-        .space  4
-        .ascii  "110110001000\000"
-        .space  3
-        .ascii  "0011001100\000"
-        .space  5
-        .ascii  "11011000100101\000"
-        .space  1
-        .ascii  "0011000001\000"
-        .space  5
-        .ascii  "11011000100100\000"
-        .space  1
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "\000"
-        .space  15
-        .ascii  "1011\000"
-        .space  11
-        .ascii  "0011110\000"
-        .space  8
-        .ascii  "101001\000"
-        .space  9
-        .ascii  "10101\000"
-        .space  10
-        .ascii  "010\000"
-        .space  12
-        .ascii  "000110\000"
-        .space  9
-        .ascii  "001101\000"
-        .space  9
-        .ascii  "1100\000"
-        .space  11
-        .ascii  "0111\000"
-        .space  11
-        .ascii  "00110010111\000"
-        .space  4
-        .ascii  "00111110\000"
-        .space  7
-        .ascii  "00010\000"
-        .space  10
-        .ascii  "000111\000"
-        .space  9
-        .ascii  "1111\000"
-        .space  11
-        .ascii  "0110\000"
-        .space  11
-        .ascii  "1101110\000"
-        .space  8
-        .ascii  "00110000001\000"
-        .space  4
-        .ascii  "0010\000"
-        .space  11
-        .ascii  "0000\000"
-        .space  11
-        .ascii  "1110\000"
-        .space  11
-        .ascii  "110101\000"
-        .space  9
-        .ascii  "11011111\000"
-        .space  7
-        .ascii  "1101000\000"
-        .space  8
-        .ascii  "1101001011\000"
-        .space  5
-        .ascii  "1101101\000"
-        .space  8
-        .ascii  "11011000101\000"
-        .space  4
-        .space  2128
-        .text
-        .align  2
-        .global encode
-        .type   encode, %function
-encode:
-        @ args = 0, pretend = 0, frame = 16
-        @ frame_needed = 1, uses_anonymous_args = 0
-        stmfd   sp!, {fp, lr}
-        add     fp, sp, #4
-        sub     sp, sp, #16
-        str     r0, [fp, #-16]
-        str     r1, [fp, #-20]
-.L2:
-        ldr     r0, [fp, #-16]
-        bl      fgetc
-        mov     r3, r0
-        strb    r3, [fp, #-5]
-        ldr     r2, .L4
-        ldrb    r3, [fp, #-5]   @ zero_extendqisi2
-        mov     r3, r3, asl #4
-        add     r3, r2, r3
-        mov     r0, r3
-        ldr     r1, [fp, #-20]
-        bl      fputs
-        b       .L2
-.L5:
-        .align  2
-.L4:
-        .word   lookup
-        .size   encode, .-encode
-        .section        .rodata
-        .align  2
+	.arch armv4t
+	.fpu softvfp
+	.eabi_attribute 20, 1
+	.eabi_attribute 21, 1
+	.eabi_attribute 23, 3
+	.eabi_attribute 24, 1
+	.eabi_attribute 25, 1
+	.eabi_attribute 26, 2
+	.eabi_attribute 30, 6
+	.eabi_attribute 18, 4
+	.file	"Encode.c"
+	.section	.rodata
+	.align	2
 .LC0:
-        .ascii  "Usage: %s <file>\012\000"
-        .align  2
+	.ascii	"100\000"
+	.align	2
 .LC1:
-        .ascii  "r\000"
-        .align  2
+	.ascii	"00111111\000"
+	.align	2
 .LC2:
-        .ascii  "Error opening file\000"
-        .align  2
+	.ascii	"11011001\000"
+	.align	2
 .LC3:
-        .ascii  "Input file opened successfully.\000"
-        .align  2
+	.ascii	"001110\000"
+	.align	2
 .LC4:
-        .ascii  "encoded.txt\000"
-        .align  2
+	.ascii	"101000\000"
+	.align	2
 .LC5:
-        .ascii  "w\000"
-        .align  2
+	.ascii	"110110000\000"
+	.align	2
 .LC6:
-        .ascii  "Error opening encoded file\000"
-        .align  2
+	.ascii	"001100010\000"
+	.align	2
 .LC7:
-        .ascii  "Output file created successfully.\000"
-        .align  2
+	.ascii	"001100011\000"
+	.align	2
 .LC8:
-        .ascii  "Encoding completed. Check 'encoded.txt'.\000"
-        .text
-        .align  2
-        .global main
-        .type   main, %function
+	.ascii	"0011001001\000"
+	.align	2
+.LC9:
+	.ascii	"1101100011\000"
+	.align	2
+.LC10:
+	.ascii	"0011001010\000"
+	.align	2
+.LC11:
+	.ascii	"0011001101\000"
+	.align	2
+.LC12:
+	.ascii	"1101001100\000"
+	.align	2
+.LC13:
+	.ascii	"110100100\000"
+	.align	2
+.LC14:
+	.ascii	"0011000010\000"
+	.align	2
+.LC15:
+	.ascii	"00110000000\000"
+	.align	2
+.LC16:
+	.ascii	"0011000011\000"
+	.align	2
+.LC17:
+	.ascii	"110100111\000"
+	.align	2
+.LC18:
+	.ascii	"001100111\000"
+	.align	2
+.LC19:
+	.ascii	"0011001000\000"
+	.align	2
+.LC20:
+	.ascii	"1101001101\000"
+	.align	2
+.LC21:
+	.ascii	"1101100010011\000"
+	.align	2
+.LC22:
+	.ascii	"1101001010\000"
+	.align	2
+.LC23:
+	.ascii	"110111100\000"
+	.align	2
+.LC24:
+	.ascii	"110111101\000"
+	.align	2
+.LC25:
+	.ascii	"00110010110\000"
+	.align	2
+.LC26:
+	.ascii	"110110001000\000"
+	.align	2
+.LC27:
+	.ascii	"0011001100\000"
+	.align	2
+.LC28:
+	.ascii	"11011000100101\000"
+	.align	2
+.LC29:
+	.ascii	"0011000001\000"
+	.align	2
+.LC30:
+	.ascii	"11011000100100\000"
+	.align	2
+.LC31:
+	.ascii	"1011\000"
+	.align	2
+.LC32:
+	.ascii	"0011110\000"
+	.align	2
+.LC33:
+	.ascii	"101001\000"
+	.align	2
+.LC34:
+	.ascii	"10101\000"
+	.align	2
+.LC35:
+	.ascii	"010\000"
+	.align	2
+.LC36:
+	.ascii	"000110\000"
+	.align	2
+.LC37:
+	.ascii	"001101\000"
+	.align	2
+.LC38:
+	.ascii	"1100\000"
+	.align	2
+.LC39:
+	.ascii	"0111\000"
+	.align	2
+.LC40:
+	.ascii	"00110010111\000"
+	.align	2
+.LC41:
+	.ascii	"00111110\000"
+	.align	2
+.LC42:
+	.ascii	"00010\000"
+	.align	2
+.LC43:
+	.ascii	"000111\000"
+	.align	2
+.LC44:
+	.ascii	"1111\000"
+	.align	2
+.LC45:
+	.ascii	"0110\000"
+	.align	2
+.LC46:
+	.ascii	"1101110\000"
+	.align	2
+.LC47:
+	.ascii	"00110000001\000"
+	.align	2
+.LC48:
+	.ascii	"0010\000"
+	.align	2
+.LC49:
+	.ascii	"0000\000"
+	.align	2
+.LC50:
+	.ascii	"1110\000"
+	.align	2
+.LC51:
+	.ascii	"110101\000"
+	.align	2
+.LC52:
+	.ascii	"11011111\000"
+	.align	2
+.LC53:
+	.ascii	"1101000\000"
+	.align	2
+.LC54:
+	.ascii	"1101001011\000"
+	.align	2
+.LC55:
+	.ascii	"1101101\000"
+	.align	2
+.LC56:
+	.ascii	"11011000101\000"
+	.data
+	.align	2
+	.type	lookup, %object
+	.size	lookup, 512
+lookup:
+	.space	128
+	.word	.LC0
+	.space	4
+	.word	.LC1
+	.space	16
+	.word	.LC2
+	.space	16
+	.word	.LC3
+	.space	4
+	.word	.LC4
+	.space	72
+	.word	.LC5
+	.word	.LC6
+	.word	.LC7
+	.word	.LC8
+	.word	.LC9
+	.word	.LC10
+	.word	.LC11
+	.word	.LC12
+	.word	.LC13
+	.word	.LC14
+	.word	.LC15
+	.word	.LC16
+	.word	.LC17
+	.word	.LC18
+	.word	.LC19
+	.word	.LC20
+	.word	.LC21
+	.word	.LC22
+	.word	.LC23
+	.word	.LC24
+	.word	.LC25
+	.word	.LC26
+	.word	.LC27
+	.word	.LC28
+	.word	.LC29
+	.word	.LC30
+	.space	24
+	.word	.LC31
+	.word	.LC32
+	.word	.LC33
+	.word	.LC34
+	.word	.LC35
+	.word	.LC36
+	.word	.LC37
+	.word	.LC38
+	.word	.LC39
+	.word	.LC40
+	.word	.LC41
+	.word	.LC42
+	.word	.LC43
+	.word	.LC44
+	.word	.LC45
+	.word	.LC46
+	.word	.LC47
+	.word	.LC48
+	.word	.LC49
+	.word	.LC50
+	.word	.LC51
+	.word	.LC52
+	.word	.LC53
+	.word	.LC54
+	.word	.LC55
+	.word	.LC56
+	.space	20
+	.text
+	.align	2
+	.global	encode
+	.type	encode, %function
+encode:
+	@ Function supports interworking.
+	@ args = 0, pretend = 0, frame = 16
+	@ frame_needed = 1, uses_anonymous_args = 0
+	stmfd	sp!, {fp, lr}
+	add	fp, sp, #4
+	sub	sp, sp, #16
+	str	r0, [fp, #-16]
+	str	r1, [fp, #-20]
+	b	.L2
+.L3:
+	ldr	r3, [fp, #-8]
+	cmp	r3, #0
+	blt	.L2
+	ldr	r3, [fp, #-8]
+	cmp	r3, #127
+	bgt	.L2
+	ldr	r3, [fp, #-8]
+	ldr	r2, .L5
+	ldr	r3, [r2, r3, asl #2]
+	cmp	r3, #0
+	beq	.L2
+	ldr	r3, [fp, #-8]
+	ldr	r2, .L5
+	ldr	r3, [r2, r3, asl #2]
+	mov	r0, r3
+	ldr	r1, [fp, #-20]
+	bl	fputs
+.L2:
+	ldr	r0, [fp, #-16]
+	bl	fgetc
+	mov	r3, r0
+	str	r3, [fp, #-8]
+	ldr	r3, [fp, #-8]
+	cmn	r3, #1
+	bne	.L3
+	sub	sp, fp, #4
+	ldmfd	sp!, {fp, lr}
+	bx	lr
+.L6:
+	.align	2
+.L5:
+	.word	lookup
+	.size	encode, .-encode
+	.section	.rodata
+	.align	2
+.LC57:
+	.ascii	"Usage: %s <file>\012\000"
+	.align	2
+.LC58:
+	.ascii	"r\000"
+	.align	2
+.LC59:
+	.ascii	"Error opening input file\000"
+	.align	2
+.LC60:
+	.ascii	"Input file opened successfully.\000"
+	.align	2
+.LC61:
+	.ascii	"encoded.txt\000"
+	.align	2
+.LC62:
+	.ascii	"w\000"
+	.align	2
+.LC63:
+	.ascii	"Error opening encoded file\000"
+	.align	2
+.LC64:
+	.ascii	"Output file created successfully.\000"
+	.align	2
+.LC65:
+	.ascii	"Encoding completed. Check 'encoded.txt'.\000"
+	.text
+	.align	2
+	.global	main
+	.type	main, %function
 main:
-        @ args = 0, pretend = 0, frame = 24
-        @ frame_needed = 1, uses_anonymous_args = 0
-        stmfd   sp!, {fp, lr}
-        add     fp, sp, #4
-        sub     sp, sp, #24
-        str     r0, [fp, #-16]
-        str     r1, [fp, #-20]
-        ldr     r3, [fp, #-16]
-        cmp     r3, #2
-        beq     .L7
-        ldr     r3, [fp, #-20]
-        ldr     r3, [r3, #0]
-        ldr     r0, .L12
-        mov     r1, r3
-        bl      printf
-        mov     r3, #1
-        str     r3, [fp, #-24]
-        b       .L8
-.L7:
-        ldr     r3, [fp, #-20]
-        add     r3, r3, #4
-        ldr     r3, [r3, #0]
-        mov     r0, r3
-        ldr     r1, .L12+4
-        bl      fopen
-        mov     r3, r0
-        str     r3, [fp, #-12]
-        ldr     r3, [fp, #-12]
-        cmp     r3, #0
-        bne     .L9
-        ldr     r0, .L12+8
-        bl      perror
-        mov     r3, #1
-        str     r3, [fp, #-24]
-        b       .L8
-.L9:
-        ldr     r0, .L12+12
-        bl      puts
-        ldr     r0, .L12+16
-        ldr     r1, .L12+20
-        bl      fopen
-        mov     r3, r0
-        str     r3, [fp, #-8]
-        ldr     r3, [fp, #-8]
-        cmp     r3, #0
-        bne     .L10
-        ldr     r0, .L12+24
-        bl      perror
-        ldr     r0, [fp, #-12]
-        bl      fclose
-        mov     r3, #1
-        str     r3, [fp, #-24]
-        b       .L8
-.L10:
-        ldr     r0, .L12+28
-        bl      puts
-        ldr     r0, [fp, #-12]
-        ldr     r1, [fp, #-8]
-        bl      encode
-        ldr     r0, [fp, #-8]
-        bl      fclose
-        ldr     r0, [fp, #-12]
-        bl      fclose
-        ldr     r0, .L12+32
-        bl      puts
-        mov     r3, #0
-        str     r3, [fp, #-24]
+	@ Function supports interworking.
+	@ args = 0, pretend = 0, frame = 24
+	@ frame_needed = 1, uses_anonymous_args = 0
+	stmfd	sp!, {fp, lr}
+	add	fp, sp, #4
+	sub	sp, sp, #24
+	str	r0, [fp, #-16]
+	str	r1, [fp, #-20]
+	ldr	r3, [fp, #-16]
+	cmp	r3, #2
+	beq	.L8
+	ldr	r3, [fp, #-20]
+	ldr	r3, [r3, #0]
+	ldr	r0, .L13
+	mov	r1, r3
+	bl	printf
+	mov	r3, #1
+	str	r3, [fp, #-24]
+	b	.L9
 .L8:
-        ldr     r3, [fp, #-24]
-        mov     r0, r3
-        sub     sp, fp, #4
-        ldmfd   sp!, {fp, pc}
+	ldr	r3, [fp, #-20]
+	add	r3, r3, #4
+	ldr	r3, [r3, #0]
+	mov	r0, r3
+	ldr	r1, .L13+4
+	bl	fopen
+	mov	r3, r0
+	str	r3, [fp, #-12]
+	ldr	r3, [fp, #-12]
+	cmp	r3, #0
+	bne	.L10
+	ldr	r0, .L13+8
+	bl	perror
+	mov	r3, #1
+	str	r3, [fp, #-24]
+	b	.L9
+.L10:
+	ldr	r0, .L13+12
+	bl	puts
+	ldr	r0, .L13+16
+	ldr	r1, .L13+20
+	bl	fopen
+	mov	r3, r0
+	str	r3, [fp, #-8]
+	ldr	r3, [fp, #-8]
+	cmp	r3, #0
+	bne	.L11
+	ldr	r0, .L13+24
+	bl	perror
+	ldr	r0, [fp, #-12]
+	bl	fclose
+	mov	r3, #1
+	str	r3, [fp, #-24]
+	b	.L9
+.L11:
+	ldr	r0, .L13+28
+	bl	puts
+	ldr	r0, [fp, #-12]
+	ldr	r1, [fp, #-8]
+	bl	encode
+	ldr	r0, [fp, #-8]
+	bl	fclose
+	ldr	r0, [fp, #-12]
+	bl	fclose
+	ldr	r0, .L13+32
+	bl	puts
+	mov	r3, #0
+	str	r3, [fp, #-24]
+.L9:
+	ldr	r3, [fp, #-24]
+	mov	r0, r3
+	sub	sp, fp, #4
+	ldmfd	sp!, {fp, lr}
+	bx	lr
+.L14:
+	.align	2
 .L13:
-        .align  2
-.L12:
-        .word   .LC0
-        .word   .LC1
-        .word   .LC2
-        .word   .LC3
-        .word   .LC4
-        .word   .LC5
-        .word   .LC6
-        .word   .LC7
-        .word   .LC8
-        .size   main, .-main
-        .ident  "GCC: (Sourcery G++ Lite 2008q3-72) 4.3.2"
-        .section        .note.GNU-stack,"",%progbits
+	.word	.LC57
+	.word	.LC58
+	.word	.LC59
+	.word	.LC60
+	.word	.LC61
+	.word	.LC62
+	.word	.LC63
+	.word	.LC64
+	.word	.LC65
+	.size	main, .-main
+	.ident	"GCC: (Sourcery G++ Lite 2008q3-72) 4.3.2"
+	.section	.note.GNU-stack,"",%progbits
